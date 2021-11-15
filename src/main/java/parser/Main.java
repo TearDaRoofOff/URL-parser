e706620f5bafb7381e6f8abc81caa50ca41d7fb1
Ruslan Kolesnikov <kolesnikov.ruslan@gmail.com>
2021-11-16 02:45:29 +0700

    How many words are in the book <<War and Peace>>?

--- src/main/java/parser/Main.java
+++ src/main/java/parser/Main.java
@@ -4,10 +4,10 @@ import parser.services.WordsCounter;
 
 public class Main {
   public static void main(String[] args) {
-    String url = "https://www.simbirsoft.com/";
+    String url = "https://introcs.cs.princeton.edu/java/data/war+peace.txt";
     WordsCounter wordsCounter = new WordsCounter();
     wordsCounter
             .counter(url)
-            .forEach((s, aLong) -> System.out.printf("\n %-40s %3d", s, aLong));
+            .forEach((s, aLong) -> System.out.printf("\n %-40s %5d", s, aLong));
   }
 }
